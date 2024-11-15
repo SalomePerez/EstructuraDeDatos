1c6761391d2afe308ae2c008786d7ee00ccc9271
DanielaMunoz29 <daniela.munoza1@uqvirtual.edu.co>
2024-11-15 16:37:42 -0500

    actualizado

--- Proyecto/src/main/java/edu/co/uniquindio/Modelo/Usuario.java
+++ Proyecto/src/main/java/edu/co/uniquindio/Modelo/Usuario.java
@@ -1,4 +1,51 @@
 package edu.co.uniquindio.Modelo;
 
 public class Usuario {
+
+    //Atributos
+    String nombre;
+    String identificacion;
+    String correo;
+    String contrasenia;
+
+    //Constructor
+    public Usuario(String nombre, String identificacion, String correo, String contrasenia) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    //Get y Set
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
