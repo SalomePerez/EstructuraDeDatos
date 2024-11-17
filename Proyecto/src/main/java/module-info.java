2eb9caf6591232532af65ce549b75447485ca782
DanielaMunoz29 <daniela.munoza1@uqvirtual.edu.co>
2024-11-17 17:35:31 -0500

    actualizacionInterfaz

--- Proyecto/src/main/java/module-info.java
+++ Proyecto/src/main/java/module-info.java
@@ -2,6 +2,21 @@ module edu.co.uniquindio.proyecto {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.media;
+    requires javafx.swing;
+
+    // Exportar el paquete de controladores para que FXML pueda acceder a él
+    exports edu.co.uniquindio.Controllers;
+    // Abrir el paquete para reflexión
+    opens edu.co.uniquindio.Controllers to javafx.fxml;
+
+    // Exportar el paquete de la aplicación principal
+    exports edu.co.uniquindio.Application;
+    opens edu.co.uniquindio.Application to javafx.fxml;
+
+    // Si tienes más paquetes que necesitan ser accedidos por FXML, agrégalos aquí
+    exports edu.co.uniquindio.Model.Principales;
+    opens edu.co.uniquindio.Model.Principales to javafx.fxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -13,8 +28,6 @@ module edu.co.uniquindio.proyecto {
     requires okhttp3;
     requires jakarta.mail;
     requires org.apache.poi.ooxml;
-    opens edu.co.uniquindio.Application to javafx.fxml;
-    exports edu.co.uniquindio.Application;
 
 
 }
